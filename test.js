b25fc751f119090fead72f6efaa97761d628a7fc
Bhawya Gulati <bhawyagulati@Bhawyas-MacBook-Air.local>
2026-05-14 12:40:41 +0530

    Update multiplication variable

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const multiply = require("./app");
 
-if (multiply(4, 5) === 20) {
+if (multiply(5, 5) === 25) {
   console.log("Pull request validation test passed");
 } else {
   throw new Error("Pull request validation test failed");
